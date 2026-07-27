38eb8badc5d59fee99d00dfd48e1dc494d67b90a
andriantopbusiness-sys <andriantopbusiness@gmail.com>
2026-07-27 20:17:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "preventivi-v8-quotecut-fix";
+const CACHE = "preventivi-v9-ui-photo-post";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).catch(() => undefined).then(() => self.skipWaiting()));
