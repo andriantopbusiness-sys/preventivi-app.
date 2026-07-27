87e4f9f1c01eea01bb0aa3fab78c38f79a664406
andriantopbusiness-sys <andriantopbusiness@gmail.com>
2026-07-27 02:15:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "preventivi-v3";
+const CACHE = "preventivi-v4-services";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
